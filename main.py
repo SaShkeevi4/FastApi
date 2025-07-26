d507f0e479106d72187cece25ae6b622182523c7
sashkeevi4 <alexsandr.01@list.ru>
2025-07-26 15:28:39 +0300

    Dynamic variables

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 @app.get("/")
-def home() -> dict[str, str]:
+async def home() -> dict[str, str]:
     return {"data": "message"}
 
 @app.get("/contacts")
-def contacts() -> int:
+async def contacts() -> int:
     return 34
+
+posts = [
+    {'id': 1, 'title': 'News 1', 'body': 'Text1'},
+    {'id': 2, 'title': 'News 2', 'body': 'Text2'},
+    {'id': 3, 'title': 'News 3', 'body': 'Text3'}
+]
+
+@app.get('/items/{id}')
+async def items(id: int) -> dict:
+    for post in posts:
+        if post['id'] == id:
+            return post
+
+    raise HTTPException(status_code=404, detail='Post not found')
