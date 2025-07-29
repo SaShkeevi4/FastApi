6aacbc552b71614ed90a1809eaa44816afb9b69f
sashkeevi4 <alexsandr.01@list.ru>
2025-07-29 16:12:50 +0300

    Pydantic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from typing import Optional
+from typing import Optional, List, Dict
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
@@ -10,26 +11,47 @@ async def home() -> dict[str, str]:
 async def contacts() -> int:
     return 34
 
-posts = [
-    {'id': 1, 'title': 'News 1', 'body': 'Text1'},
-    {'id': 2, 'title': 'News 2', 'body': 'Text2'},
-    {'id': 3, 'title': 'News 3', 'body': 'Text3'}
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+class Post(BaseModel):
+    id: int
+    title: str 
+    body: str
+    author: User
+
+users = [
+    {'id': 1, 'name': 'Jone', 'age': 24},
+    {'id': 2, 'name': 'Alex', 'age': 30},
+    {'id': 3, 'name': 'Bob', 'age': 56}
 ]
 
+posts = [
+    {'id': 1, 'title': 'News 1', 'body': 'Text1', 'author': users[1]},
+    {'id': 2, 'title': 'News 2', 'body': 'Text2', 'author': users[2]},
+    {'id': 3, 'title': 'News 3', 'body': 'Text3', 'author': users[0]}
+]
+
+@app.get('/items')
+async def items() -> List[Post]:
+    return [Post(**post) for post in posts]
+
 @app.get('/items/{id}')
-async def items(id: int) -> dict:
+async def items(id: int) -> Post:
     for post in posts:
         if post['id'] == id:
-            return post
+            return Post(**post)
 
     raise HTTPException(status_code=404, detail='Post not found')
 
 @app.get("/search")
-async def search(post_id: Optional[int] = None) -> dict:
+async def search(post_id: Optional[int] = None) -> Dict[str, Optional[Post]]:
     if post_id:
         for post in posts:
             if post['id'] == post_id:
-                return post
+                return {"data": Post(**post)}
         raise HTTPException(status_code=404, detail='Post not found')
     else:
-        return{"data": "No post id provided"}
+        return{"data": None}
